5eb74fd58c8cf6fd2ce2a3061da710c90bdb924c
Se-myung-Jin <tpaud94@naver.com>
2024-07-22 01:58:31 +0900

    [파일추가] SocketAsyncEventArgs 확장 메서드 추가

---
+++ NetCoreServer/001. ServerCore/ServerCore/99. Extension/SocketAsyncEventArgsEx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace ServerCore
+{
+    public static class SocketAsyncEventArgsEx
+    {
+        public static bool IsRecvSuccess(this SocketAsyncEventArgs args)
+        {
+            return (args.SocketError == SocketError.Success && args.BytesTransferred > 0);
+        }
+    }
+}
