e81f77500db269e5cf0874ed233492909c86d846
Se-myung-Jin <tpaud94@naver.com>
2024-07-24 16:07:48 +0900

    [코드정리] SocketAsyncEventArgsEx 코드 정리

--- NetCoreServer/001. ServerCore/ServerCore/99. Extension/SocketAsyncEventArgsEx.cs
+++ NetCoreServer/001. ServerCore/ServerCore/99. Extension/SocketAsyncEventArgsEx.cs
@@ -6,7 +6,7 @@ namespace ServerCore
 {
     public static class SocketAsyncEventArgsEx
     {
-        public static bool IsRecvSuccess(this SocketAsyncEventArgs args)
+        public static bool IsTransferSuccess(this SocketAsyncEventArgs args)
         {
             return (args.SocketError == SocketError.Success && args.BytesTransferred > 0);
         }
