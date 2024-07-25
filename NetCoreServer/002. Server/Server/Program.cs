790fa92204f9d4e82d01390bb14bf7d77834a621
Se-myung-Jin <tpaud94@naver.com>
2024-07-26 00:06:48 +0900

    [파일추가] 서버 및 더미 클라이언트 테스트 코드 추가

---
+++ NetCoreServer/002. Server/Server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Text;
+using ServerCore;
+
+namespace Server
+{
+
+    public class GameSession : Session
+    {
+        public override void OnConnected(EndPoint e)
+        {
+            Console.WriteLine($"OnConnected : {e}");
+
+            byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+            Send(sendBuff);
+            Thread.Sleep(1000);
+            Disconnect();
+        }
+
+        public override void OnDisconnected(EndPoint e)
+        {
+            Console.WriteLine($"OnDisconnected : {e}");
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Client] {recvData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName();
+            IPHostEntry entry = Dns.GetHostEntry(host);
+            IPAddress ipAddr = entry.AddressList[0];
+            IPEndPoint endpoint = new IPEndPoint(ipAddr, 8994);
+
+            Listener listener = new Listener();
+            listener.StartListening(endpoint, () => { return new GameSession(); });
+
+            Console.WriteLine("Listening...");
+
+            try
+            {
+                while (true)
+                {
+                    ;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ NetCoreServer/003. Client/DummyClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ServerCore;
+
+namespace DummyClient
+{
+    class Program
+    {
+        public class GameSession : Session
+        {
+            public override void OnConnected(EndPoint e)
+            {
+                Console.WriteLine($"OnConnected : {e}");
+
+
+                for (int i = 0; i < 5; i++)
+                {
+                    byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i}");
+                    Send(sendBuff);
+                }
+            }
+
+            public override void OnDisconnected(EndPoint e)
+            {
+                Console.WriteLine($"OnDisconnected : {e}");
+            }
+
+            public override void OnRecv(ArraySegment<byte> buffer)
+            {
+                string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+                Console.WriteLine($"[From Server] {recvData}");
+            }
+
+            public override void OnSend(int numOfBytes)
+            {
+                Console.WriteLine($"Transferred bytes: {numOfBytes}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName();
+            IPHostEntry entry = Dns.GetHostEntry(host);
+            IPAddress ipAddr = entry.AddressList[0];
+            IPEndPoint endpoint = new IPEndPoint(ipAddr, 8994);
+
+            Connector connector = new Connector();
+            connector.Connect(endpoint, () => { return new GameSession(); });
+
+            while (true)
+            {
+                try
+                {
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                Thread.Sleep(100);
+            }
+            
+        }
+    }
+}
