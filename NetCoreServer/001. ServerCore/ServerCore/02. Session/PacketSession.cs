c8950a6ef1758ebbd64cfb5d2629fe53f466b3bd
Se-myung-Jin <tpaud94@naver.com>
2024-07-29 02:18:37 +0900

    [파일추가] PacketSession 추가

---
+++ NetCoreServer/001. ServerCore/ServerCore/02. Session/PacketSession.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    public abstract class PacketSession : Session
+    {
+        public static readonly int HeaderSize = 2;
+
+        public override int OnRecv(ArraySegment<byte> buffer)
+        {
+            int processLen = 0;
+
+            while (true)
+            {
+                if (buffer.Count < HeaderSize) break;
+
+                ushort packetSize = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+                if (buffer.Count < packetSize) break;
+
+                OnParsePacket(new ArraySegment<byte>(buffer.Array, buffer.Offset, packetSize));
+
+                processLen += packetSize;
+                buffer = new ArraySegment<byte>(buffer.Array, buffer.Offset + packetSize, buffer.Count - packetSize);
+            }
+
+            return processLen;
+        }
+
+        public abstract void OnParsePacket(ArraySegment<byte> buffer);
+    }
+}
