f3e7f030c6cb0cbe9aadf84f5865aafceeadf67d
Se-myung-Jin <tpaud94@naver.com>
2024-08-10 22:14:53 +0900

    [파일추가] ClientSession 추가

---
+++ NetCoreServer/002. Server/Server/03. Session/ClientSession.cs
@@ -0,0 +1,43 @@
+﻿using ServerCore;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    public class ClientSession : PacketSession
+    {
+        public int SessionId { get; set; }
+        public GameRoom Room { get; set; }
+
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            Program.Room.Push(() => Program.Room.Enter(this));
+        }
+
+        public override void OnParsePacket(ArraySegment<byte> buffer)
+        {
+            PacketManager.Instance.OnParsePacket(this, buffer);
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            SessionManager.Instance.RemoveSession(this);
+
+            if (Room != null )
+            {
+                GameRoom room = Room;
+                room.Push(() => room.Leave(this));
+                Room = null;
+            }
+
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            //Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+}
---
+++ NetCoreServer/002. Server/Server/03. Session/SessionManager.cs
@@ -0,0 +1,46 @@
+﻿namespace Server
+{
+    class SessionManager
+    {
+        static SessionManager instance = new SessionManager();
+        public static SessionManager Instance { get { return instance; } }
+
+        int sessionId = 0;
+        Dictionary<int, ClientSession> sessionDic = new Dictionary<int, ClientSession>();
+        object sessionLock = new object();
+
+        public ClientSession GenerateSession()
+        {
+            lock (sessionLock)
+            {
+                int sessionId = ++this.sessionId;
+
+                ClientSession session = new ClientSession();
+                session.SessionId = sessionId;
+                sessionDic.Add(sessionId, session);
+
+                Console.WriteLine($"Connected : {sessionId}");
+                return session;
+            }
+        }
+
+        public ClientSession Find(int id)
+        {
+            lock (sessionLock)
+            {
+                ClientSession session = null;
+                sessionDic.TryGetValue(id, out session);
+                
+                return session;
+            }
+        }
+
+        public void RemoveSession(ClientSession session)
+        {
+            lock (sessionLock)
+            {
+                sessionDic.Remove(session.SessionId);
+            }
+        }
+    }
+}
