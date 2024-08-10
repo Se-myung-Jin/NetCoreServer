a07f9ab4c7e30f2ebe10f1ffe86d8ca567c5094a
Se-myung-Jin <tpaud94@naver.com>
2024-08-10 22:12:26 +0900

    [파일추가] SendBuffer 추가

---
+++ NetCoreServer/001. ServerCore/ServerCore/04. Util/SendBuffer.cs
@@ -0,0 +1,58 @@
+﻿namespace ServerCore
+{
+    public class SendBufferHelper
+    {
+        public static ThreadLocal<SendBuffer> CurrentBuffer = new ThreadLocal<SendBuffer>(() => { return null; });
+
+        public static int ChunkSize { get; set; } = 65535 * 100;
+
+        public static ArraySegment<byte> Open(int reserveSize)
+        {
+            if (CurrentBuffer.Value == null)
+            {
+                CurrentBuffer.Value = new SendBuffer(ChunkSize);
+            }
+            
+            if (CurrentBuffer.Value.FreeSize < reserveSize)
+            {
+                CurrentBuffer.Value = new SendBuffer(ChunkSize);
+            }
+
+            return CurrentBuffer.Value.Open(reserveSize);
+        }
+
+        public static ArraySegment<byte> Close(int usedSize)
+        {
+            return CurrentBuffer.Value.Close(usedSize);
+        }
+    }
+
+    public class SendBuffer
+    {
+        byte[] buffer;
+        int usedSize = 0;
+
+        public int FreeSize { get { return buffer.Length - usedSize; } }
+
+        public SendBuffer(int chunkSize)
+        {
+            buffer = new byte[chunkSize];
+        }
+
+        public ArraySegment<byte> Open(int reserveSize)
+        {
+            if (reserveSize > FreeSize) return null;
+
+            return new ArraySegment<byte>(buffer, usedSize, reserveSize);
+        }
+
+        public ArraySegment<byte> Close(int usedSize)
+        {
+            ArraySegment<byte> segment = new ArraySegment<byte>(buffer, this.usedSize, usedSize);
+
+            this.usedSize += usedSize;
+
+            return segment;
+        }
+    }
+}
