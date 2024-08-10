654782ccbaa9597e101d3001c640face3eb40f5b
Se-myung-Jin <tpaud94@naver.com>
2024-08-10 22:14:37 +0900

    [파일추가] Room 추가

---
+++ NetCoreServer/002. Server/Server/02. Room/GameRoom.cs
@@ -0,0 +1,49 @@
+﻿using ServerCore;
+
+namespace Server
+{
+    public class GameRoom : IRoom<ClientSession>, IJobQueue
+    {
+        List<ClientSession> sessionList = new List<ClientSession>();
+        JobQueue jobQueue = new JobQueue();
+        List<ArraySegment<byte>> pendingList = new List<ArraySegment<byte>>();
+
+        public void Broadcast(ClientSession session, string chat)
+        {
+            ChatRes res = new ChatRes();
+            res.PlayerId = session.SessionId;
+            res.Chat = $"{chat} I am {res.PlayerId}";
+
+            ArraySegment<byte> segment = res.WritePacket();
+
+            pendingList.Add(segment);
+        }
+
+        public void Enter(ClientSession session)
+        {
+            sessionList.Add(session);
+            session.Room = this;
+        }
+
+        public void Leave(ClientSession session)
+        {
+            sessionList.Remove(session);
+        }
+
+        public void Push(Action job)
+        {
+            jobQueue.Push(job);
+        }
+
+        public void Flush()
+        {
+            foreach (ClientSession session in sessionList)
+            {
+                session.Send(pendingList);
+            }
+
+            Console.WriteLine($"Flush Memory Count : {pendingList.Count}");
+            pendingList.Clear();
+        }
+    }
+}
---
+++ NetCoreServer/002. Server/Server/02. Room/IRoom.cs
@@ -0,0 +1,10 @@
+﻿using ServerCore;
+
+namespace Server
+{
+    public interface IRoom<T> where T : PacketSession
+    {
+        void Enter(T session);
+        void Leave(T session);
+    }
+}
